8b5a455cdf0dee54a4b9733a2bbbec5eb2f8b75b
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2019-12-26 11:15:17 +0700

    Add files via upload

---
+++ pointerE1.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define god cout<<"\n";
+struct Person
+{
+	string name,nametoo;
+	int num;
+	Person() : name(), nametoo(), num(){}
+	Person(string n, string n2, int n3) : name(n), nametoo(n2), num(n3){}
+};
+void db(int n)
+{
+	n*=2;
+}
+int dbp(int &n)
+{
+	return n*=2;
+}
+int dbn(int const& n)
+{
+	return n+10;
+}
+int main()
+{
+	int A=5;
+	int* pA=&A;
+	cout << pA << " " << *pA;
+	int *pB=new int(6);
+	god
+	cout << ++(*pB);
+	god
+	Person Music("Music","Musictoo",17);
+	Person *pMusic = &Music;
+	cout << (*pMusic).nametoo << " " << pMusic->nametoo;
+	god
+	int a=5;
+	cout << dbp(a) << " " << dbn(19);
+	//cout << dbp(19);
+}
---
+++ pointerE2.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define god printf("\n");
+int main()
+{
+	int i=5;
+	int j=10;
+	int *pI = &i;
+
+	int const *pcI = pI; //pointer to a const
+	// you can't use "*pcI=4"
+	pcI = &j;
+	cout << *pcI;god
+
+	int * const cpI = pI; //constant pointer
+	*cpI=4; //This is acceptable
+	// cpI = &j => This is unacceptable
+	cout << *cpI << " " << *pI;god //value pointed by &pI is also change.
+
+	int const * const crazy = pI; //const pointer to a const
+	//*crazy=4;
+	//crazy=&j;
+	//both above are unacceptable.
+	int kk = *crazy;
+	cout << kk;god
+}
