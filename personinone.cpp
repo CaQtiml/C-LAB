0f9ea867c39e4d92924ab7beeadcc49c9c80b249
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2019-12-27 09:34:35 +0700

    Add files via upload

--- personinone.cpp
+++ personinone.cpp
@@ -8,16 +8,24 @@ private:
 public:
 	Person(string f,string la,int num); //constructor
 	Person()=default; //constructor too
+	Person(int num);
 	string getName();
+	int getNum(int k);
 };
 Person::Person(string f,string la,int num) : fn(f),ln(la),num(num){}
+Person::Person(int num) : num(num){}
 string Person::getName()
 {
 	return fn + " " + ln;
+}
+int Person::getNum(int k)
+{
+	return num*k;
 }
 int main()
 {
 	Person p1("Sivakorn","LT",17);
 	Person p2;
-	cout << p1.getName();
+	Person p3(5);
+	cout << p1.getName() << " " << p3.getNum(5);
 }
