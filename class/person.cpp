da6c800ccab08568c19bb6dabcb77ab5c13b4939
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2019-12-28 22:14:12 +0700

    Add files via upload

--- class/person.cpp
+++ class/person.cpp
@@ -1,14 +1,37 @@
+
 #include "person.h"
 #include "bits/stdc++.h"
 using namespace std;
+#define PKhing printf("\n")
 //Person::Person(string f,string la,int num) : fn(f),ln(la),num(num){}
 //string Person::getName()
 /*{
 	return fn + " " + ln;
 }*/
+bool Person::operator<(Person const& p)const
+{
+	return num<p.num;
+}
+bool Person::operator<(int i)const
+{
+	return num<i;
+}
+bool operator<(int i,Person const& p)
+{
+	return i<p.getNumber();
+	// return i<p.num; //Accepted only when there is (*) line in person.h
+}
+
 int main()
 {
-	Person p1("SL","LT",17);
+	Person p1("SS","Jeam",112);
+	Person p2("O","Mhdol",67);
 	//Person p2;
-	cout << p1.getName();
+	//cout << p1.getName();
+	p1<p2 ? cout << "T" : cout << "F";
+	PKhing;
+	p1<300 ? cout << "TT" : cout << "FF";
+	PKhing;
+	224<p1 ? cout << "TTT" : cout << "FFF";
+
 }
--- class/person.h
+++ class/person.h
@@ -5,11 +5,19 @@ class Person
 private:
 	string fn,ln;
 	int num;
+	//friend bool operator<(int i,Person const& p); ---(*)
 public:
+	//Person(string f,string la,int num);
 	Person(string f,string la,int num) : fn(f),ln(la),num(num){} //constructor
 	Person(); //constructor too
 	string getName()
 	{
 		return fn+" "+ln;
 	}
+	int getNumber() const {return num;}
+	bool operator<(Person const& p) const;
+	bool operator<(int i) const;
 };
+bool operator<(int i,Person const& p);
+
+//const will call function that has "const"
