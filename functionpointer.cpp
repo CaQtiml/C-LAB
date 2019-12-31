09912f50d58f7fcdc494b426dad7ac6e14431cf9
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2019-12-31 16:08:28 +0700

    Add files via upload

---
+++ functionpointer.cpp
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "iostream"
+using namespace std;
+int add(int a,int b){return a+b;}
+void PHC(){printf("HELLO\n");}
+void PH(char *name){printf("HELLO%s\n",name);}
+int main()
+{
+	int (*p)(int,int) = &add;
+	//int (*p)(int,int) = add; //This is also acceptable because function name will return as a pointer
+	int c = (*p)(2,3); //de-referencing and executing the function
+	cout << c << "\n";
+
+	void (*ptr)() = PHC;
+	ptr();
+
+	void (*ptr2)(char*) = PH;
+	ptr2("MUSIC");
+}
